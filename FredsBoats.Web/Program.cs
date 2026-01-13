ae22fa8fede16624ba1115e47d2221e55de0d98a
st024808 <st024808@stud.frederick.ac.cy>
2026-01-13 12:47:16 +0000

    answer

--- FredsBoats.Web/Program.cs
+++ FredsBoats.Web/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using FredsBoats.Web.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -22,12 +25,9 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
 app.Run();
